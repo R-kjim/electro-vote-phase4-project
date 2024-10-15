bc48dad189aec870b8ef26e627c2759082713c16
ESTON NDUNGU <estonndungu1@gmail.com>
2024-10-15 15:09:43 +0300

    Create a nav bar and add styling to it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
 import LoginSignup from './components/LoginSignup';
-import Navbar from './components/Navbar';
+
 
 const App = () => {
   return (
