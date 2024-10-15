1aa6ed652a4b381aa8b1eeec6f693f2bedd893cb
Anthony-Maundu <anthonymaundu1@gmail.com>
2024-10-15 10:53:41 +0300

    links registration icon in Homepage to login/register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login-signup" element={<LoginSignup />} />
+        {/* Add more routes here as needed */}
       </Routes>
     </Router>
   );
